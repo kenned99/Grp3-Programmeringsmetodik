60d24081859eca3aa86e5b6924a6a8d4849a1ada
Seppi hr. Chef <SHDa.skp@edu.mercantec.dk>
2021-01-27 10:42:20 +0100

    Classes and DB context

---
+++ ServerSide/Model/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerSide.Model
+{
+    public class Drink
+    {
+        public Drink()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public List<Category> Category { get; set; }
+        public string Description { get; set; }
+        public string[] Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public Byte[] Image { get; set; }
+        public User Author { get; set; }
+
+    }
+}
