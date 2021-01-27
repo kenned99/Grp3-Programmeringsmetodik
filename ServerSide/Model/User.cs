f314ff19ab255b7fca396e1fd22689b7ac8172f9
Seppi hr. Chef <SHDa.skp@edu.mercantec.dk>
2021-01-27 10:53:07 +0100

    Forgot to save

--- ServerSide/Model/User.cs
+++ ServerSide/Model/User.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ServerSide.Model
 {
-    class User
+    public class User
     {
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Byte[] ProfileImage { get; set; }
     }
 }
