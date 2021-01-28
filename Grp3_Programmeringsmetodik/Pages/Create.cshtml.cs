d92c9e1712b50412e0e2cfe208295d105a157502
Andreas259 <arm@ewe.dk>
2021-01-28 11:08:18 +0100

    Add code behind to create page

--- Grp3_Programmeringsmetodik/Pages/Create.cshtml.cs
+++ Grp3_Programmeringsmetodik/Pages/Create.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerSide;
+using ServerSide.Model;
 
 namespace Grp3_Programmeringsmetodik.Pages
 {
     public class CreateModel : PageModel
     {
+        private readonly IDataController idc;
+
+        public CreateModel(IDataController idc)
+        {
+            this.idc = idc;
+        }
+        [BindProperty]
+        public Drink drink { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {     
+            idc.CreateDrink(drink, Request.Form["categoryID"]);
+            idc.Commit();
         }
     }
 }
