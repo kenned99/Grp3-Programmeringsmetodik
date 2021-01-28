ccf84cd42a7070e47f0a9d7d335a9e4b969f435f
Lunar <nk1s.skp>
2021-01-28 13:50:57 +0100

    Img on Index, names in Category

--- Grp3_Programmeringsmetodik/Pages/Category.cshtml.cs
+++ Grp3_Programmeringsmetodik/Pages/Category.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerSide;
+using ServerSide.Model;
 
 namespace Grp3_Programmeringsmetodik.Pages
 {
     public class CategoryModel : PageModel
     {
+        private readonly IDataController idc;
+        
+        public CategoryModel(IDataController idc)
+        {
+            this.idc = idc;
+        }
+
+        public List<Drink> drinks = new List<Drink>();
+
         public void OnGet()
         {
+            for (int i = 0; i < 8; i++)
+            {
+                drinks.Add(idc.ReadDrink(i.ToString()));
+            }
         }
     }
 }
