c7501c3119b8e35f13dcfda5b0b3e5c83865e3f0
Sacha Fanefjord <sdfa.skp@edu.mercantec.dk>
2021-01-28 08:51:40 +0100

    layout drink card

---
+++ Grp3_Programmeringsmetodik/Pages/drink_card.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grp3_Programmeringsmetodik.Pages
+{
+    public class Drink_cardModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
